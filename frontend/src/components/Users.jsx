22aaeeb7e86f1196abfe59dae0efec208cb1f1df
deepak <2001dkmaurya@gmail.com>
2024-01-29 01:12:31 +0530

    Partial frontend done

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -1,19 +1,27 @@
-import { useState } from "react"
+import { useState,useEffect } from "react"
 import Button from "./Button";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 const Users = () => {
     // Replace with backend call
-    const [users, setUsers] = useState([{
-        firstName: "Harkirat",
-        lastName: "Singh",
-        _id: 1
-    }]);
+    const [users, setUsers] = useState([]);
+    const [filter, setFilter] = useState("");
+
+    useEffect(() => {
+        axios.get("http://localhost:3000/api/v1/users/bulk?filter=" + filter)
+            .then(response => {
+                setUsers(response.data.user)
+            })
+    }, [filter])
 
     return <>
         <div className="font-bold mt-6 text-lg">
             Users
         </div>
         <div className="my-2">
-            <input type="text" placeholder="Search users..." className="w-full px-2 py-1 border rounded border-slate-200"></input>
+            <input onChange={(e) => {
+                setFilter(e.target.value)
+            }} type="text" placeholder="Search users..." className="w-full px-2 py-1 border rounded border-slate-200"></input>
         </div>
         <div>
             {users.map(user => <User key={user._id} user={user} />)}
@@ -22,6 +30,8 @@ const Users = () => {
 }
 
 function User({user}) {
+    const navigate = useNavigate();
+
     return <div className="flex justify-between">
         <div className="flex">
             <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
@@ -37,7 +47,9 @@ function User({user}) {
         </div>
 
         <div className="flex flex-col justify-center h-ful">
-            <Button label={"Send Money"} />
+            <Button onClick={(e) => {
+                navigate("/send?id=" + user._id + "&name=" + user.firstName);
+            }} label={"Send Money"} />
         </div>
     </div>
 }
